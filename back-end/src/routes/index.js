4321fcc458a0eca5d35da1d8326a5d1e8601e1a4
Shaun Wassell <shaun@trove.ai>
2021-04-10 19:47:45 -0400

    Added a log in route to the server

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,7 +1,9 @@
+import { logInRoute } from './logInRoute';
 import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
+    logInRoute,
     signUpRoute,
     testRoute,
 ];
---
+++ back-end/src/routes/logInRoute.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { getDbConnection } from '../db';
+
+export const logInRoute = {
+    path: '/api/login',
+    method: 'post',
+    handler: async (req, res) => {
+        const { email, password } = req.body;
+
+        const db = getDbConnection('react-auth-db');
+        const user = await db.collection('users').findOne({ email });
+        
+        if (!user) return res.status(401); 
+        // There is no user with that email. We want to return the same
+        // status code so that people can't fish around for emails to see
+        // who has an account and who doesn't
+
+        const { _id: id, isVerified, passwordHash, info } = user;
+        
+        const isCorrect = await bcrypt.compare(password, passwordHash);
+
+        if (isCorrect) {
+            jwt.sign({ id, isVerified, email, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                res.status(200).send({ token });
+            });
+        } else {
+            res.sendStatus(401); // This is the "not authenticated" status code - remember it!
+        }
+    }
+}
