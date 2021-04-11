acc79f5c0ee6cd00b7e7f07d7c15337218a7a1e3
Shaun Wassell <shaun@trove.ai>
2021-04-11 17:34:02 -0400

    User Info Page makes requests to the server to update data

--- front-end/src/pages/UserInfoPage.js
+++ front-end/src/pages/UserInfoPage.js
@@ -1,16 +1,24 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { useToken } from '../auth/useToken';
+import { useUser } from '../auth/useUser';
 
 export const UserInfoPage = () => {
-    // We'll use the history to navigate the user
-    // programmatically later on (we're not using it yet)
-    const history = useHistory();
+    const user = useUser();
+    const [token, setToken] = useToken();
+
+    const { id, email, info } = user;
 
     // These states are bound to the values of the text inputs
     // on the page (see JSX below). 
-    const [favoriteFood, setFavoriteFood] = useState('');
-    const [hairColor, setHairColor] = useState('');
-    const [bio, setBio] = useState('');
+    const [favoriteFood, setFavoriteFood] = useState(info.favoriteFood || '');
+    const [hairColor, setHairColor] = useState(info.hairColor || '');
+    const [bio, setBio] = useState(info.bio || '');
+
+    // We'll use the history to navigate the user
+    // programmatically later on (we're not using it yet)
+    const history = useHistory();
 
     // These state variables control whether or not we show
     // the success and error message sections after making
@@ -31,10 +39,20 @@ export const UserInfoPage = () => {
     }, [showSuccessMessage, showErrorMessage]);
 
     const saveChanges = async () => {
-        // Send a request to the server to
-        // update the user's info with any changes we've
-        // made to the text input values
-        alert('Save functionality not implemented yet');
+        try {
+            const response = await axios.put(`/api/users/${id}`, {
+                favoriteFood,
+                hairColor,
+                bio,
+            }, {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            const { token: newToken } = response.data;
+            setToken(newToken);
+            setShowSuccessMessage(true);
+        } catch (error) {
+            setShowErrorMessage(true);
+        }
     }
 
     const logOut = () => {
@@ -44,15 +62,15 @@ export const UserInfoPage = () => {
     }
     
     const resetValues = () => {
-        // Reset the text input values to
-        // their starting values (the data we loaded from the server)
-        alert('Reset functionality not implemented yet');
+        setFavoriteFood(info.favoriteFood);
+        setHairColor(info.hairColor);
+        setBio(info.bio);
     }
     
     // And here we have the JSX for our component. It's pretty straightforward
     return (
         <div className="content-container">
-            <h1>Info for ______</h1>
+            <h1>Info for {email}</h1>
             {showSuccessMessage && <div className="success">Successfully saved user data!</div>}
             {showErrorMessage && <div className="fail">Uh oh... something went wrong and we couldn't save changes</div>}
             <label>
