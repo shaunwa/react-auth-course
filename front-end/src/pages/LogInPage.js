0a7cceec5ee6d0924e288a54ae37f94c1c7f7838
Shaun Wassell <shaun@trove.ai>
2021-04-11 16:53:34 -0400

    Login page now makes request to back-end

--- front-end/src/pages/LogInPage.js
+++ front-end/src/pages/LogInPage.js
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { useToken } from '../auth/useToken';
 
 export const LogInPage = () => {
+    const [token, setToken] = useToken();
+
     const [errorMessage, setErrorMessage] = useState('');
 
     const [emailValue, setEmailValue] = useState('');
@@ -10,8 +14,13 @@ export const LogInPage = () => {
     const history = useHistory();
 
     const onLogInClicked = async () => {
-        // This is where we'll send a request to the server to log in
-        alert('Login not implemented yet');
+        const response = await axios.post('/api/login', {
+            email: emailValue,
+            password: passwordValue,
+        });
+        const { token } = response.data;
+        setToken(token);
+        history.push('/');
     }
 
     return (
