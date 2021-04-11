43db28831f9250578addc14bd6ab78efd29f18f0
Shaun Wassell <shaun@trove.ai>
2021-04-11 17:33:35 -0400

    Added route for updating users

---
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -0,0 +1,51 @@
+import jwt from 'jsonwebtoken';
+import { ObjectID } from 'mongodb';
+import { getDbConnection } from '../db';
+
+export const updateUserInfoRoute = {
+    path: '/api/users/:userId',
+    method: 'put',
+    handler: async (req, res) => {
+        const { authorization } = req.headers;
+        const { userId } = req.params;
+        const updates = (({
+            favoriteFood,
+            hairColor,
+            bio,
+        }) => ({
+            favoriteFood,
+            hairColor,
+            bio,
+        }))(req.body); // A slick way of subsetting an object
+
+        if (!authorization) {
+            return res.status(401).json({ message: 'No authorization header sent' });
+        }
+
+        // Because authorization is "Bearer asdfkljasdfl.asdfasdf8as9f7a8s.asdfa89d7f89" - we need to remove "Bearer"
+        const [, token] = authorization.split(' ');
+
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) return res.status(401).json({ message: 'Unable to verify token' });
+            
+            const { id } = decoded;
+
+            if (id !== userId) return res.status(403).json({ message: 'Not allowed to update that user\'s data' });
+
+            const db = getDbConnection('react-auth-db');
+            const result = await db.collection('users').findOneAndUpdate(
+                { _id: ObjectID(id) },
+                { $set: { info: updates } },
+                { returnOriginal: false },
+            ); 
+            const { email, isVerified, info } = result.value;
+            
+            jwt.sign({ id, email, isVerified, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                res.status(200).json({ token });
+            });
+        });
+    },
+};
