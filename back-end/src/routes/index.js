94607d1b3359a25596bf5594da198cf9d5be993c
Shaun Wassell <shaun@trove.ai>
2021-04-12 19:21:59 -0400

    Added reset password route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,6 @@
 import { forgotPasswordRoute } from './forgotPasswordRoute';
 import { logInRoute } from './logInRoute';
+import { resetPasswordRoute } from './resetPasswordRoute';
 import { signUpRoute } from './signUpRoute';
 import { testEmailRoute } from './testEmailRoute';
 import { testRoute } from './testRoute';
@@ -9,6 +10,7 @@ import { verifyEmailRoute } from './verifyEmailRoute';
 export const routes = [
     forgotPasswordRoute,
     logInRoute,
+    resetPasswordRoute,
     signUpRoute,
     testEmailRoute,
     testRoute,
---
+++ back-end/src/routes/resetPasswordRoute.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import { sendEmail } from '../util/sendEmail';
+import { getDbConnection } from '../db';
+
+export const resetPasswordRoute = {
+    path: '/api/users/:passwordResetCode/reset-password',
+    method: 'put',
+    handler: async (req, res) => {
+        const { passwordResetCode } = req.params;
+        const { newPassword } = req.body;
+
+        const db = getDbConnection('react-auth-db');
+
+        const newPasswordHash = await bcrypt.hash(newPassword, 10);
+
+        const result = await db.collection('users').findOneAndUpdate({ passwordResetCode }, {
+            $set: { passwordHash: newPasswordHash },
+            $unset: { passwordResetCode: '' },
+        });
+
+        if (result.lastErrorObject.n === 0) return res.sendStatus(404);
+
+        res.sendStatus(200);
+    },
+};
