6c22bb2d90201d662400ed45fdf1369614717140
Shaun Wassell <shaun@trove.ai>
2021-04-12 21:10:50 -0400

    Overhauled reset password route

--- back-end/src/routes/resetPasswordRoute.js
+++ back-end/src/routes/resetPasswordRoute.js
@@ -1,26 +1,22 @@
-import bcrypt from 'bcrypt';
-import { v4 as uuid } from 'uuid';
-import { sendEmail } from '../util/sendEmail';
-import { getDbConnection } from '../db';
+import { CognitoUser } from 'amazon-cognito-identity-js';
+import { awsUserPool } from '../util/awsUserPool';
 
 export const resetPasswordRoute = {
-    path: '/api/users/:passwordResetCode/reset-password',
+    path: '/api/users/:confirmationCode/reset-password',
     method: 'put',
     handler: async (req, res) => {
-        const { passwordResetCode } = req.params;
-        const { newPassword } = req.body;
+        const { confirmationCode } = req.params;
+        const { email, newPassword } = req.body;
+        console.log({ email, confirmationCode, newPassword });
 
-        const db = getDbConnection('react-auth-db');
-
-        const newPasswordHash = await bcrypt.hash(newPassword, 10);
-
-        const result = await db.collection('users').findOneAndUpdate({ passwordResetCode }, {
-            $set: { passwordHash: newPasswordHash },
-            $unset: { passwordResetCode: '' },
-        });
-
-        if (result.lastErrorObject.n === 0) return res.sendStatus(404);
-
-        res.sendStatus(200);
+        new CognitoUser({ Username: email, Pool: awsUserPool })
+            .confirmPassword(confirmationCode, newPassword, {
+                onSuccess: result => {
+                    res.sendStatus(200);
+                },
+                onFailure: result => {
+                    res.sendStatus(401);
+                },
+            });
     },
 };
