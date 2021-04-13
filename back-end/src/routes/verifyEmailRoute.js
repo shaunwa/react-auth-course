4447a26c647f0043deeed402e86b5cb11f8ee042
Shaun Wassell <shaun@trove.ai>
2021-04-12 20:44:35 -0400

    Rewrote verify email route to fit with AWS

--- back-end/src/routes/verifyEmailRoute.js
+++ back-end/src/routes/verifyEmailRoute.js
@@ -1,27 +1,31 @@
 import { ObjectID } from 'mongodb';
 import jwt from 'jsonwebtoken';
+import { CognitoUser } from 'amazon-cognito-identity-js';
 import { getDbConnection } from '../db';
+import { awsUserPool } from '../util/awsUserPool';
 
 export const verifyEmailRoute = {
     path: '/api/verify-email',
     method: 'put',
     handler: async (req, res) => {
-        const { verificationString } = req.body;
+        const { email, verificationString } = req.body;
 
-        const db = await getDbConnection('react-auth-db');
-        const result = await db.collection('users').findOne({ verificationString }); 
-        
-        if (!result) return res.status(401).json({ message: 'The email verification code is incorrect' });
+        new CognitoUser({ Username: email, Pool: awsUserPool })
+            .confirmRegistration(verificationString, true, async (err) => {
+                if (err) return res.status(401).json({ message: 'The email verification code is incorrect' });
 
-        const { _id: id, email, info } = result;
+                const db = getDbConnection('react-auth-db');
 
-        await db.collection('users').updateOne({ _id: ObjectID(id) }, { $set: { isVerified: true } }); 
+                const result = await db.collection('users')
+                    .findOneAndUpdate({ email }, { $set: { isVerified: true } }, { returnOriginal: false }); 
+                const { _id: id, isVerfied, info } = result.value;
 
-        jwt.sign({ id, email, isVerified: true, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
-            if (err) {
-                return res.status(500).json(err);
-            }
-            res.status(200).json({ token });
-        });
+                jwt.sign({ id, email, isVerified: true, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                    if (err) {
+                        return res.status(500).json(err);
+                    }
+                    res.status(200).json({ token });
+                });
+            });
     },
 };
